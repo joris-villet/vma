a106973191f2b144f43f22753c04e8560cd42024
joris-villet <jorisvillet@gmail.com>
2022-06-06 16:39:21 +0200

    calcul distance et temps ok

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,7 @@ const app = {
   currentSpeed: 14,
   targetSpeed: 0,
   targetVma: 0,
-  arraySpeeds: [],
+  arrayDistances: [100, 200, 300, 400, 500, 600, 800, 1000, 1500, 2000],
 
   elem: function() {
     return {
@@ -21,20 +21,21 @@ const app = {
 
   
   AllEventsListeners: function() {
-    console.log("input => ", this.elem())
+    // console.log("input => ", this.elem())
     const el = this.elem();
 
     el.inputs.addEventListener('input', (e) => {
-      // console.log(e.target)
+      // console.log(e.target.value)
 
       switch (e.target.id) {
         case 'speed':
           el.targetSpeed.textContent = e.target.value + ' km/h';
-          this.targetSpeed = parseInt(e.target.value, 10);
+          // this.targetSpeed = parseInt(e.target.value, 10);
+          this.targetSpeed = Number(e.target.value);
           break;
         case 'purcent':
           el.targetVma.textContent = e.target.value + ' %';
-          this.targetVma = parseInt(e.target.value, 10);
+          this.targetVma = Number(e.target.value);
           break;
         default:
           null;
@@ -45,12 +46,17 @@ const app = {
   },
 
   calcul: function () {
-    console.log(this.targetSpeed)
-    console.log(this.targetVma)
+    // console.log(this.targetSpeed)
+    // console.log(this.targetVma)
     const resultSpeed = this.getSpeed(this.targetVma, this.targetSpeed);
-    console.log(resultSpeed)
-    const times = this.getTimePerSpeed(resultSpeed);
-    console.log("temps de passage au 400 m => ", times)
+
+    this.arrayDistances.forEach(distance => {
+      // console.log(distance);
+      const times = this.getTimePerSpeed(resultSpeed, distance);
+      console.log(`temps de passage au ${distance} m => `, times);
+      this.createElements(distance, times);
+    })
+
   },
 
   getSpeed: function (purcent, speed) {
@@ -59,14 +65,33 @@ const app = {
     return result.toFixed(1);
   },
 
-  getTimePerSpeed: function (speed) {
+  getTimePerSpeed: function (speed, distance) {
      // VITESSE EN M/S = vitesse / 3.6;
     // TEMPS = DISTANCE / VITESSE
     console.log("speed ", speed)
     speed /= 3.6;
-    let time = 200 / speed.toFixed(2);
-    time = Math.round(time.toFixed(2));
-    return time;
+    let time = distance / speed.toFixed(2);
+    time = Math.round(time);
+    let minutes = Math.floor(time / 60);
+    let secondes = time % 60;
+    console.log("minutes ", minutes)
+    console.log("secondes ", secondes)
+    if (secondes < 10) secondes = '0' + secondes;
+    return minutes < 1 ? secondes + ' sec' : minutes + '.' + secondes + ' min';
+    //return minutes + ' ' + secondes;
+  },
+
+
+  createElements: function (distance, time) {
+    // let elem = document.createElement(element);
+    // elem.textContent = content;
+    let tr = document.createElement('tr');
+    tr.innerHTML = `
+      <td>${distance} M</td>
+      <td>${time}</td>
+    `;
+    const el = this.elem();
+    el.table__speed.appendChild(tr);
   },
 
 
@@ -81,12 +106,6 @@ const app = {
 }
 
 
-
-// function getSpeed(purcent, speed) {
-//   let result = null;
-//   result = speed * purcent / 100;
-//   return result.toFixed(1);
-// }
 
 
 
